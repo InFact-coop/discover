acbe2404e619c0cebf00caccce5997d911da18b6
Abdallah <abdallah.halees@gmail.com>
2018-11-05 10:51:48 +0200

    add axios and move types to separate folder

--- src/state/actions/title.js
+++ src/state/actions/title.js
@@ -1,16 +1,22 @@
+const axios = require("axios")
+import {
+  CHANGE_TEXT_COLOR,
+  CHANGE_TEXT_TO_BLACK,
+  CHANGE_TEXT_TO_WHITE,
+} from "../types"
+
 const changeTextColorAction = color => ({
-  type: "CHANGE_TEXT_COLOR",
+  type: CHANGE_TEXT_COLOR,
   payload: color,
   offline: {
-    commit: { type: "CHANGE_TEXT_TO_WHITE" },
-    rollback: { type: "CHANGE_TEXT_TO_BLACK" },
+    commit: { type: CHANGE_TEXT_TO_WHITE },
+    rollback: { type: CHANGE_TEXT_TO_BLACK },
   },
 })
 
 export default () => dispatch => {
-  fetch("/api/random")
-    .then(res => res.json())
-    .then(({ color }) => {
+  axios("/api/random")
+    .then(({ data: { color } }) => {
       dispatch(changeTextColorAction(color))
     })
     .catch(() => {
--- src/state/reducers/title.js
+++ src/state/reducers/title.js
@@ -1,13 +1,19 @@
+import {
+  CHANGE_TEXT_COLOR,
+  CHANGE_TEXT_TO_BLACK,
+  CHANGE_TEXT_TO_WHITE,
+} from "../types"
+
 const initState = {
   textColor: "#ffffff",
 }
 export default (state = initState, action) => {
   switch (action.type) {
-    case "CHANGE_TEXT_COLOR":
+    case CHANGE_TEXT_COLOR:
       return { ...state, textColor: action.payload }
-    case "CHANGE_TEXT_TO_WHITE":
+    case CHANGE_TEXT_TO_WHITE:
       return { ...state, textColor: "#ffffff" }
-    case "CHANGE_TEXT_TO_BLACK":
+    case CHANGE_TEXT_TO_BLACK:
       return { ...state, textColor: "#000000" }
     default:
       return state
