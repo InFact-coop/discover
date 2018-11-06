535d6e68fd788c8be0c9e8643c10bf24d0656dcb
Abdallah <abdallah.halees@gmail.com>
2018-11-06 13:03:42 +0200

    add access codes model and controllers

---
+++ server/controllers/accessCode.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken")
+const AccessCode = require("../models/AccessCode")
+const { secret } = require("../config/config")
+
+exports.generateCode = (req, res) => {
+  const randomCode = Math.random()
+    .toString(36)
+    .substr(2, 10)
+
+  AccessCode.create({ code: randomCode })
+    .then(docs => {
+      res.json(docs)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+}
+
+exports.verifyCode = (req, res) => {
+  const { code } = req.body
+  AccessCode.findOne({ code })
+    .then(docs => {
+      if (!docs || docs.verified || Date.now() > docs.expires_at)
+        throw new Error("Code isn't valid, please check with")
+      return AccessCode.findOneAndUpdate(
+        { code },
+        { verified: true },
+        { new: true }
+      )
+    })
+    .then(({ code }) => {
+      const token = jwt.sign({ code }, secret)
+      res.json({ token })
+    })
+    .catch(err => {
+      const message = err.message || "Something is wrong please check with"
+      res.status(500).json({ err: true, message })
+    })
+}
