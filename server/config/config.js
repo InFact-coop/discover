c8a5f26adbc43f7012f3670f19c2b0c5bb1dba9e
Abdallah <abdallah.halees@gmail.com>
2018-11-06 13:02:47 +0200

    add config file for differnet envs

---
+++ server/config/config.js
@@ -0,0 +1,29 @@
+require("env2")("./.env")
+const {
+  NODE_ENV,
+  DEVELOPMENT_DATABASE,
+  PRODUCTION_DATABASE,
+  SECRET,
+  ADMIN_ACCESS_TOKEN,
+} = process.env
+
+const config = env => {
+  if (env === "development") {
+    return {
+      database: DEVELOPMENT_DATABASE,
+      secret: SECRET,
+      adminAccessToken: ADMIN_ACCESS_TOKEN,
+    }
+  } else if (env === "production") {
+    return {
+      database: PRODUCTION_DATABASE,
+      secret: SECRET,
+    }
+  }
+  return {
+    database: DEVELOPMENT_DATABASE,
+    secret: SECRET,
+    adminAccessToken: ADMIN_ACCESS_TOKEN,
+  }
+}
+module.exports = config(NODE_ENV)
