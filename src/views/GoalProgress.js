699bd6d29d1205c3a6d1d64b45f8ab16c6d0b384
Jessie Beech <jessie@jessiebeech.com>
2018-11-13 11:26:41 +0000

    created header and button components Relates #89

--- src/views/GoalProgress.js
+++ src/views/GoalProgress.js
@@ -2,9 +2,17 @@ import { Component } from "react"
 import { connect } from "react-redux"
 import { changeView } from "../state/actions/router"
 
+import IconHeader from "../components/shared/IconHeader"
+import GoalProgressList from "../components/GoalProgressList"
+import ActionButton from "../components/shared/ActionButton"
+
+import icon from "../assets/icons/list_of_goals_big.svg"
+
 const GoalProgress = () => (
   <div>
-    <h1 className="font-1 sans">Goals</h1>
+    <IconHeader title="Goals progress" icon={icon}/>
+    <GoalProgressList />
+    <ActionButton>Set new goal</ActionButton>
   </div>
 )
 
