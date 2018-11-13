1e6aa63ec776a27e80507feb0d91bc189d9ccc3b
Jessie Beech <jessie@jessiebeech.com>
2018-11-13 12:38:12 +0000

    create styled components for goal items/progress bars Relates #89

--- src/components/GoalProgressList.js
+++ src/components/GoalProgressList.js
@@ -1,10 +1,80 @@
 import { Component } from "react"
+import styled from "styled-components"
 
+const _Wrapper = styled.div.attrs({
+  className: "ph3 flex flex-column items-center mb5"
+})``
+
+const _GoalText = styled.h2.attrs({
+  className: "w-100 font-4 sans dark-gray mb2"
+})``
+
+const _ProgressBar = styled.div.attrs({
+  className: "w-100 ba b--moon-gray br3 tc relative"
+})`
+  height: 20px;
+`
+const _ProgressFill = styled.div.attrs({
+  className: ""
+})`
+  border-radius: 7px;
+  height: 18px;
+  width: ${({width}) => `${width}`};
+  background-color: ${({color}) => `var(--${color})`};
+  opacity: 0.5;
+`
+const _ProgressText = styled.h2.attrs({
+  className: "mono font-5 dark-gray absolute top-0"
+})`
+  left: 50%;
+  transform: translateX(-50%);
+  line-height: 20px;
+`
+
+class Goal extends Component {
+  render () {
+    const { width, color, goal, progressText } = this.props
+    return (
+      <div className="mb3">
+        <_GoalText>{goal}</_GoalText>
+        <_ProgressBar>
+          <_ProgressFill width={width} color={color}/>
+          <_ProgressText>{progressText}</_ProgressText>
+        </_ProgressBar> 
+      </div>
+    )
+  }
+}
 
 export default class GoalProgressList extends Component {
   render () {
     return (
-      <h2>goal</h2>
+      <_Wrapper>
+        <Goal 
+          width="80%" 
+          color="purple" 
+          goal="Stop Procrastinating and work on my time management skills"
+          progressText="40 days out of 60"
+        />
+        <Goal 
+          width="50%" 
+          color="green" 
+          goal="Stop Procrastinating and work on my time management skills"
+          progressText="28 days out of 30"
+          />
+        <Goal 
+          width="100%" 
+          color="blue" 
+          goal="Stop Procrastinating and work on my time management skills"
+          progressText="Huzzah! Completed!"
+          />
+         <Goal 
+          width="100%" 
+          color="light-red" 
+          goal="Stop Procrastinating and work on my time management skills"
+          progressText="Huzzah! Completed!"
+          />
+      </_Wrapper>
     )
   }
 }
