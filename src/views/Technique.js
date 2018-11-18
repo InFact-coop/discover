3634486acd74a35fc3f77492d3bbb51e0a386611
Abdallah <abdallah.halees@gmail.com>
2018-11-18 11:38:00 +0200

    add progress bar, save and back button

--- src/views/Technique.js
+++ src/views/Technique.js
@@ -1,7 +1,12 @@
 import { Component, Fragment } from "react"
 import { connect } from "react-redux"
 import styled, { createGlobalStyle } from "styled-components"
-import { toggleSelectTechnique } from "../state/actions/technique"
+import PropTypes from "prop-types"
+import ProgressBar from "../components/ProgressBar"
+import SkipButton from "../components/SkipButton"
+import SaveButton from "../components/SaveButton"
+import { SetGoal, GoalDays } from "."
+import { changeTechniques } from "../state/actions/currentGoal"
 import Card from "../components/Card"
 import Carousel from "../components/Carousel"
 import background from "../assets/backgrounds/bg_which_technique.svg"
@@ -14,7 +19,7 @@ const GlobalStyle = createGlobalStyle`
 const _Container = styled.div.attrs({
   className: "flex flex-column items-center",
 })`
-  margin-top: 30%;
+  margin-top: 10%;
 `
 
 const _Title = styled.p.attrs({
@@ -37,16 +42,47 @@ const _Hint = styled.p.attrs({
 `
 
 class Technique extends Component {
+  state = {
+    techniques: [],
+  }
+  componentDidMount() {
+    const { staticData, currentGoal } = this.props
+    this.setState({
+      techniques: staticData.techniques.map(t => ({
+        ...t,
+        selected: currentGoal.techniques.includes(t.title),
+      })),
+    })
+  }
   onCardClick = title => () => {
-    const { toggleSelectTechnique } = this.props
-    toggleSelectTechnique(title)
+    const { techniques } = this.state
+    this.setState({
+      techniques: techniques.map(technique => {
+        const { selected } = technique
+        if (technique.title === title) {
+          technique.selected = !selected
+        }
+        return technique
+      }),
+    })
+  }
+
+  saveFunction = () => {
+    const { changeTechniques } = this.props
+    const { techniques } = this.state
+    const selectedTechniques = techniques
+      .filter(({ selected }) => selected)
+      .map(({ title }) => title)
+    changeTechniques(selectedTechniques)
   }
   render() {
-    const { techniques } = this.props
+    const { techniques } = this.state
     return (
       <Fragment>
         <GlobalStyle />
         <_Container>
+          <ProgressBar progress={3} />
+          <SkipButton action="back" to={SetGoal} />
           <_Title> Great!</_Title>
           <_Question>
             and which technique will you choose to achieve your goal?
@@ -54,25 +90,39 @@ class Technique extends Component {
           <_Hint>&#40;you can choose more than one!&#41;</_Hint>
         </_Container>
         <Carousel>
-          {techniques.map(({ title, description, image, selected }) => (
-            <Card
-              key={title}
-              width="17rem"
-              height="22rem"
-              title={title}
-              description={description}
-              image={image}
-              selected={selected}
-              onCardClick={this.onCardClick(title)}
-            />
-          ))}
+          {techniques.map(
+            ({ title, description, image, backgroundColor, selected }) => (
+              <Card
+                key={title}
+                width="17rem"
+                height="22rem"
+                title={title}
+                description={description}
+                image={image}
+                backgroundColor={backgroundColor}
+                selected={selected}
+                onCardClick={this.onCardClick(title)}
+              />
+            )
+          )}
         </Carousel>
+        <SaveButton
+          saveFunction={this.saveFunction}
+          redirectTo={GoalDays}
+          text="NEXT"
+        />
       </Fragment>
     )
   }
 }
 
+Technique.prototypes = {
+  staticData: PropTypes.object.isRequired,
+  currentGoal: PropTypes.object,
+  changeTechniques: PropTypes.func.isRequired,
+}
+
 export default connect(
-  ({ technique: { techniques } }) => ({ techniques }),
-  { toggleSelectTechnique }
+  ({ staticData, currentGoal }) => ({ staticData, currentGoal }),
+  { changeTechniques }
 )(Technique)
