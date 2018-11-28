2ab2ef390f87fb1c916fc26b00c5a6c887ef817c
Ivan <ivangonzalez@rocketmail.com>
2018-11-28 10:50:24 +0000

    Add initial controller for dialog-flow

---
+++ server/controllers/dialogFlow.js
@@ -0,0 +1,64 @@
+const dialogflow = require("dialogflow")
+const r = require("ramda")
+
+const sessionClient = new dialogflow.SessionsClient({
+  credentials: JSON.parse(process.env.GCS_KEYFILE),
+})
+const projectId = "discover-b86c1"
+const sessionId = "lsadjfaslasdjfasdlfjjdf"
+const sessionPath = sessionClient.sessionPath(projectId, sessionId)
+const languageCode = "en-GB"
+
+const valuesPath = [
+  0,
+  "queryResult",
+  "fulfillmentMessages",
+  0,
+  "payload",
+  "fields",
+]
+
+const getPayload = r.pipe(
+  r.view(r.lensPath([...valuesPath, "responses", "listValue", "values"])),
+  r.map(r.prop("stringValue"))
+)
+
+const getPostback = r.pipe(
+  r.view(r.lensPath([...valuesPath, "postback", "structValue", "fields"])),
+  r.map(postback => {
+    const { kind } = postback
+    return postback[kind]
+  })
+)
+
+exports.queryDF = (req, res) => {
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: req.query.query,
+        languageCode,
+      },
+    },
+  }
+
+  // Send request and log result
+  sessionClient
+    .detectIntent(request)
+    .then(responses => {
+      // console.log(getPostback(responses))
+      // console.log(JSON.stringify(responses))
+      // if (result.intent) {
+      //   // console.log(`  Intent: ${result.intent.displayName}`)
+      // } else {
+      //   // console.log(`  No intent matched.`)
+      // }
+      res.json({
+        responses: getPayload(responses),
+        postback: getPostback(responses),
+      })
+    })
+    .catch(err => {
+      // console.error("ERROR:", err)
+    })
+}
