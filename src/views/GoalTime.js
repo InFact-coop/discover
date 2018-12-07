7ba99e529873b4cda460fcf2685440ad0df7e637
Jessie Beech <jessie@jessiebeech.com>
2018-12-07 12:13:53 +0000

    carousel initial index to selected goal

--- src/views/GoalTime.js
+++ src/views/GoalTime.js
@@ -104,6 +104,7 @@ class GoalTime extends Component {
     const { times, hours, minutes } = this.state
     const {
       changeView,
+      timeOfDay,
       router: { history },
     } = this.props
     const edit = history[history.length - 1] === "EditGoal"
@@ -129,7 +130,11 @@ class GoalTime extends Component {
             Okay, And when do you think you are most likely to work on your
             goal?
           </_Description>
-          <Carousel>
+          <Carousel
+            initialIndex={times
+              .map(t => t.title)
+              .indexOf(timeOfDay.description)}
+          >
             {times.map(({ title, image, backgroundColor, selected }) => (
               <Card
                 key={title}
