d2b8197de6bcb7d38ae4b1d2ac5c0a6e8f21d1b0
Ivan <ivangonzalez@rocketmail.com>
2018-12-08 11:51:41 +0000

    Fix wraparound on carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,37 +1,33 @@
 import { Component } from "react"
 import styled from "styled-components"
 import PropTypes from "prop-types"
+import Flickity from "react-flickity-component"
 
 const _FlickityContainer = styled.div.attrs({
   className: "w-100",
 })``
 
 class Carousel extends Component {
-  state = { Flickity: null }
-  constructor(props) {
-    super(props)
-    if (typeof window !== "undefined") {
-      this.state.Flickity = require("react-flickity-component")
-    }
+  componentDidUpdate() {
+    this.flkty.resize()
   }
+
   render() {
-    const { Flickity } = this.state
     const { children, initialIndex } = this.props
     return (
       <_FlickityContainer>
-        {Flickity && (
-          <Flickity
-            options={{
-              prevNextButtons: false,
-              wrapAround: true,
-              initialIndex: initialIndex || 0,
-            }}
-            className={"outline-0 w-100 overflow-hidden"}
-            disableImagesLoaded={true}
-          >
-            {children}
-          </Flickity>
-        )}
+        <Flickity
+          flickityRef={c => (this.flkty = c)}
+          options={{
+            wrapAround: true,
+            prevNextButtons: false,
+            initialIndex: initialIndex || 0,
+          }}
+          className="outline-0 w-100 overflow-hidden"
+          disableImagesLoaded={true}
+        >
+          {children}
+        </Flickity>
       </_FlickityContainer>
     )
   }
--- src/components/shared/IconHeader.js
+++ src/components/shared/IconHeader.js
@@ -1,32 +1,32 @@
-import styled from "styled-components";
+import styled from "styled-components"
 import { Component } from "react"
 
 const _Wrapper = styled.div.attrs({
-  className: "flex flex-column items-center justify-center w-100 pv3"
+  className: "flex flex-column items-center justify-center w-100 pv3",
 })``
 
 const _RoundIcon = styled.div.attrs({
-  className: "bg-center"
+  className: "bg-center",
 })`
   width: 85px;
   height: 85px;
-  background: ${({icon}) => `url(${icon})`};
+  background: ${({ icon }) => `url(${icon})`};
 `
 
 const _Heading = styled.h1.attrs({
-  className: "sans font-4 fw5 dark-gray"
+  className: "sans font-4 fw5 dark-gray mv2",
 })``
 
 class IconHeader extends Component {
-  render () {
+  render() {
     const { title, icon } = this.props
     return (
       <_Wrapper>
-        <_RoundIcon icon={icon}/>
+        <_RoundIcon icon={icon} />
         <_Heading>{title}</_Heading>
-      </_Wrapper> 
+      </_Wrapper>
     )
-  } 
+  }
 }
 
 export default IconHeader
