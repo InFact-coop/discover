6d081379881d9f25b54d57e37a813314d33a79a7
lucymk <lucy.m.king92@gmail.com>
2018-12-12 11:40:33 +0000

    add postcss clean

--- postcss.config.js
+++ postcss.config.js
@@ -7,5 +7,6 @@ module.exports = {
     require("autoprefixer"), // adds vendor prefixes like -webkit or -moz to your css for better support
     require("postcss-custom-media"), // can set media queries as variables
     require("postcss-custom-properties"), // allows you to use css variables e.g. var(--my-variable)
+    require("postcss-clean"), // minifies the output css (i.e. removes all the spaces and comments)
   ],
 }
